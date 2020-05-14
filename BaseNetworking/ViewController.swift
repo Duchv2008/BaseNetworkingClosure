152d9d28b50d857b4ba678f59cba3fe9703c328c
duchv2008 <duchv2008@gmail.com>
2020-05-14 21:19:53 +0700

    Initial Commit

---
+++ BaseNetworking/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BaseNetworking
+//
+//  Created by Nhocbangchu95 on 5/14/20.
+//  Copyright © 2020 Nhocbangchu95. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
